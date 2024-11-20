0b0a2e9b760513ef6d80ca0541ad309007a369b6
juansantos <juan.pablo.santos.2014@gmail.com>
2024-11-20 17:53:15 -0300

    verifcar con w el cart

--- src/components/common/carWidget/CartWidget.jsx
+++ src/components/common/carWidget/CartWidget.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../../../context/CartContext";
-import "./cartWidget.css";
+import "./CartWidget.css";
 import IconButton from "@mui/material/IconButton";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Badge from "@mui/material/Badge";
